de3a256ec99524d1ed97620e549f9775557fc928
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-21 13:04:21 +0600

    using fetching methods from Facade

--- webapp/src/main/java/kz/theeurasia/policy/calc/bean/facade/DefaultDriverFacade.java
+++ webapp/src/main/java/kz/theeurasia/policy/calc/bean/facade/DefaultDriverFacade.java
@@ -4,20 +4,8 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import com.lapsa.insurance.domain.ContactData;
-import com.lapsa.insurance.domain.IdentityCardData;
-import com.lapsa.insurance.domain.OriginData;
-import com.lapsa.insurance.domain.PersonalData;
-import com.lapsa.insurance.domain.ResidenceData;
 import com.lapsa.insurance.domain.policy.Policy;
 import com.lapsa.insurance.domain.policy.PolicyDriver;
-import com.lapsa.insurance.elements.InsuranceClassType;
-import com.lapsa.insurance.elements.InsuredAgeClass;
-import com.lapsa.insurance.esbd.domain.entities.general.SubjectPersonEntity;
-import com.lapsa.insurance.esbd.services.InvalidInputParameter;
-import com.lapsa.insurance.esbd.services.NotFound;
-import com.lapsa.insurance.esbd.services.elements.InsuranceClassTypeServiceDAO;
-import com.lapsa.insurance.esbd.services.general.SubjectPersonServiceDAO;
 
 import kz.theeurasia.policy.calc.api.DriverFacade;
 import kz.theeurasia.policy.calc.api.MessageBundleCode;
@@ -30,10 +18,7 @@ public class DefaultDriverFacade implements DriverFacade {
     private static final long serialVersionUID = 1L;
 
     @Inject
-    private SubjectPersonServiceDAO subjectPersonService;
-
-    @Inject
-    private InsuranceClassTypeServiceDAO insuranceClassTypeService;
+    private com.lapsa.insurance.services.domain.PolicyDriverFacade facade;
 
     @Override
     public PolicyDriver add(Policy policy) throws ValidationException {
@@ -41,7 +26,7 @@ public class DefaultDriverFacade implements DriverFacade {
 	    throw new ValidationException(MessageBundleCode.ONLY_ONE_DRIVER_ALLOWED);
 	PolicyDriver e = new PolicyDriver();
 	policy.getInsuredDrivers().add(e);
-	_reset(policy, e);
+	_reset(e);
 	return e;
     }
 
@@ -54,72 +39,11 @@ public class DefaultDriverFacade implements DriverFacade {
 
     @Override
     public void fetchInfo(Policy policy, PolicyDriver driver) throws ValidationException {
-	try {
-	    SubjectPersonEntity fetched = subjectPersonService.getByIIN(driver.getIdNumber());
-	    driver.setFetched(true);
-
-	    driver.getPersonalData().setName(fetched.getPersonal().getName());
-	    driver.getPersonalData().setSurename(fetched.getPersonal().getSurename());
-	    driver.getPersonalData().setPatronymic(fetched.getPersonal().getPatronymic());
-	    driver.getPersonalData().setSex(fetched.getPersonal().getSex());
-	    driver.getPersonalData().setDayOfBirth(fetched.getPersonal().getDayOfBirth().getTime());
-
-	    driver.getResidenceData().setResident(fetched.getOrigin().isResident());
-	    driver.getResidenceData().setAddress(fetched.getContact().getHomeAdress());
-
-	    // TODO Здесь потенциальная проблема связанная с тем несоответстием
-	    // мапирования
-	    if (fetched.getOrigin().getCity() != null)
-		driver.getResidenceData().setCity(fetched.getOrigin().getCity());
-	    driver.getOriginData().setCountry(fetched.getOrigin().getCountry());
-
-	    if (fetched.getIdentityCard().getDateOfIssue() != null)
-		driver.getIdentityCardData().setDateOfIssue(fetched.getIdentityCard().getDateOfIssue().getTime());
-
-	    driver.getIdentityCardData().setType(fetched.getIdentityCard().getIdentityCardType());
-	    driver.getIdentityCardData().setIssuingAuthority(fetched.getIdentityCard().getIssuingAuthority());
-	    driver.getIdentityCardData().setNumber(fetched.getIdentityCard().getNumber());
-
-	    driver.setTaxPayerNumber(fetched.getTaxPayerNumber());
-
-	    driver.getContactData().setEmail(fetched.getContact().getEmail());
-	    driver.getContactData().setPhone(fetched.getContact().getPhone());
-	    driver.getContactData().setSiteUrl(fetched.getContact().getSiteUrl());
-
-	    try {
-		InsuranceClassType insuranceClassType = insuranceClassTypeService.getForSubject(fetched);
-		driver.setInsuranceClassType(insuranceClassType);
-	    } catch (NotFound | InvalidInputParameter e) {
-		driver.setInsuranceClassType(insuranceClassTypeService.getDefault());
-	    }
-	    if (fetched.getPersonal().getDayOfBirth() != null) {
-		int years = CalculatorUtil.calculateAgeByDOB(driver.getPersonalData().getDayOfBirth());
-		driver.setAgeClass(years > 25 ? InsuredAgeClass.OVER25 : InsuredAgeClass.UNDER25);
-	    }
-
-	} catch (NotFound e) {
-	    _resetFetchedInfo(policy, driver);
-	    driver.setInsuranceClassType(insuranceClassTypeService.getDefault());
-	} catch (InvalidInputParameter e1) {
-	    _resetFetchedInfo(policy, driver);
-	}
+	facade.fetch(driver);
     }
 
-    private void _reset(Policy policy, PolicyDriver driver) {
-	_resetFetchedInfo(policy, driver);
+    private void _reset(PolicyDriver driver) {
+	facade.clearFetched(driver);
 	driver.setExpirienceClass(null);
     }
-
-    private void _resetFetchedInfo(Policy policy, PolicyDriver driver) {
-	driver.setFetched(false);
-	driver.setPersonalData(new PersonalData());
-	driver.setResidenceData(new ResidenceData());
-	driver.setOriginData(new OriginData());
-	driver.setIdentityCardData(new IdentityCardData());
-	driver.setTaxPayerNumber(null);
-	driver.setContactData(new ContactData());
-	driver.setInsuranceClassType(null);
-	driver.setAgeClass(null);
-    }
-
 }
