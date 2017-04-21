9f8f83dd69b9edb00e9eaa002de63cd74c70dd01
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-21 13:04:05 +0600

    calculation method api changed

--- webapp/src/main/java/kz/theeurasia/policy/calc/bean/facade/DefaultCalculationFacade.java
+++ webapp/src/main/java/kz/theeurasia/policy/calc/bean/facade/DefaultCalculationFacade.java
@@ -1,10 +1,14 @@
 package kz.theeurasia.policy.calc.bean.facade;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.lapsa.insurance.domain.policy.Policy;
+import com.lapsa.insurance.services.other.CalculationFailed;
 import com.lapsa.insurance.services.other.PolicyCalculationService;
 
 import kz.theeurasia.policy.calc.api.CalculationFacade;
@@ -16,10 +20,18 @@ public class DefaultCalculationFacade implements CalculationFacade {
     private static final long serialVersionUID = 1L;
 
     @Inject
+    private Logger logger;
+
+    @Inject
     private PolicyCalculationService calculationService;
 
     @Override
     public void calculatePremiumCost(Policy policy) {
-	calculationService.calculatePolicyCost(policy);
+	try {
+	    calculationService.calculatePolicyCost(policy);
+	} catch (CalculationFailed e) {
+	    logger.log(Level.FINER, "Calculation failed", e);
+	    policy.getCalculation().setCalculatedPremiumCost(0);
+	}
     }
 }
